bb0b673f180440383a87f1a2aa8444cbd45442d7
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-11-23 23:33:57 -0300

    refactor: change MONGODB_PORT to MONGODB_HOST

--- src/configs/env/env.config.ts
+++ src/configs/env/env.config.ts
@@ -3,4 +3,4 @@ import 'dotenv/config';
 export const SERVER_PORT = process.env.SERVER_PORT || 3000;
 export const MONGO_USER = process.env.MONGODB_USER || 'root';
 export const MONGO_PASSWORD = process.env.MONGODB_PASSWORD || '123';
-export const MONGO_PORT = process.env.MONGODB_PORT || 27017;
+export const MONGO_HOST = process.env.MONGODB_HOST || 'localhost';
