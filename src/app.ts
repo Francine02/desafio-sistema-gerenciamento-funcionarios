e09bb69111f9575f54bef712cfbeb0ffd1c456b2
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-11-23 21:24:59 -0300

    feat: add company and employee routes and errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
-import { swaggerDoc } from './configs/swagger.config';
+import swaggerFile from './configs/swagger/swagger-output.json';
+import { CompanyRoutes } from './modules/company/routes/company.route';
+import { EmployeeRoutes } from './modules/employee/routes/employee.route';
+import { errorHandler } from './shared/middlewares/error-handler.middleware';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(CompanyRoutes);
+app.use(EmployeeRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(errorHandler);
 
 export default app;
